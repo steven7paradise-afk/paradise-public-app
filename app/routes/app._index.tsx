474753ba175cd741a67e8b2235b56e4548f257f1
steven7paradise-afk <steven7paradise@gmail.com>
2026-05-21 15:13:24 +0200

    Redirect app home without Shopify session

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,10 +1,7 @@
 import type { LoaderFunctionArgs } from "react-router";
 import { redirect } from "react-router";
 
-import { authenticate } from "../shopify.server";
-
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
   const query = new URL(request.url).searchParams.toString();
   throw redirect(`/app/campaigns${query ? `?${query}` : ""}`);
 };
